ff2f3f0063e7cc5ae80f4511e6978e8f3c676697
Emmanuel Adelakun <devtenotea@gmail.com>
2024-05-24 12:54:44 +0100

    chore: inline fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../assets/index.css';
-import Head from 'next/head';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: any) {
+  console.log(pageProps);
   return (
     <main>
       <Head>
@@ -13,13 +14,32 @@ export default function MyApp({ Component, pageProps }: any) {
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        <style
+          nonce={pageProps.nonce}
+          dangerouslySetInnerHTML={{
+            __html: `
+        html {
+          background: black;
+        }
+
+        main {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        `,
+          }}
+        ></style>
+
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
+          nonce={pageProps.nonce}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
+          nonce={pageProps.nonce}
         />
       </Head>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-export default function HomePage() {
+import { GetServerSideProps } from "next";
+
+export default function HomePage(props: any) {
   return (
     <main>
-      <h1 style={{ backgroundColor: 'red' }}>Content goes her</h1>
+      <h1 className="bg-red" nonce={props.nonce}>
+        Content goes her
+      </h1>
     </main>
   );
 }
+
+export const getServerSideProps = (async ({ req }) => {
+  const nonce = new Headers(req.headers as any).get("x-nonce");
+  return { props: { nonce: nonce?.toString() } };
+}) satisfies GetServerSideProps<{ nonce?: string }>;
